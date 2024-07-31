c4c743bb87cccc21970363a09afa9b0cd22f1886
SRINJOY DAS <115316528+srinju@users.noreply.github.com>
2024-07-31 20:03:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
-import { useState } from 'react'
+import React, { Suspense, useState } from 'react'
 import './App.css'
-import Home from './components/Home'
+import { BrowserRouter, Routes , Route, useNavigate, useLocation , Navigate } from 'react-router-dom'
+import { AuthProvider,useAuth } from './context/Authcontext';
+import Home from './components/Home';
+
+const SignUp = React.lazy(() => import('./components/signup'));
+const Login = React.lazy(() => import('./components/login'));
+const HomePage = React.lazy(() => import('./components/Home'));
+
+function ProtectedRoute({children}) {
+  const {token} = useAuth();
+  return token ? children : <Navigate to="/login" />;
+}
 
 function App() {
-  
-
   return (
-    <div>
-      <Home id="home" />
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<SignUpOrLogin />} />
+          <Route path='/signup' element={<Suspense fallback={"loading..."}><SignUp /></Suspense>} />
+          <Route path='/login' element={<Suspense fallback={"loading..."}><Login /></Suspense>} />
+          <Route path='/home' element={
+            <ProtectedRoute>
+              <Suspense fallback={"loading..."}><HomePage /></Suspense>
+            </ProtectedRoute>
+          } />
+        </Routes>
+     </AuthProvider>
+    </BrowserRouter>
+    
   )
 }
 
-export default App
+function SignUpOrLogin() {
+  const {token} = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <>
+      {location.pathname === '/' && !token && (
+        <div>
+          <button onClick={() => navigate('/signup')}>SignUp</button>
+          <button onClick={() => navigate('/login')}>Login</button>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default App;
